21c7096cf332d7d29426bf0396ec6cc8521224e8
santosh-marar <st.juniordeveloper@gmail.com>
2025-03-31 16:02:41 +0545

    refactor: remove calendar component

--- trpc/middleware/try-catch.middleware.tsx
+++
@@ -1,8 +0,0 @@
-// const errorHandlingMiddleware = t.middleware(async ({ next }) => {
-//   try {
-//     return await next();
-//   } catch (error) {
-//     // Add universal error handling logic here
-//     throw error;
-//   }
-// });
